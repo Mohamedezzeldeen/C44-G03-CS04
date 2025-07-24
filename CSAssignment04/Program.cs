388234b536fb810c9cc74a0bd4ba0efd6c99a2cb
mohamed_ezz <mohamed_ezz@gmail.com>
2025-07-24 07:10:06 +0300

    Q2

--- CSAssignment04/Program.cs
+++ CSAssignment04/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             #region Q1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
-            
+
             //Console.Write("Enter The Number : ");
             //bool Flage = int.TryParse(Console.ReadLine(), out int Num);
             //if (Flage) 
@@ -18,7 +18,24 @@
             //    }
             //}
 
-            
+
+            #endregion
+
+            #region Q2- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+            //Console.Write("Enter The Number : ");
+            //bool Flage = int.TryParse(Console.ReadLine(), out int Num);
+            //int MP = 0;
+            //if (Flage) 
+            //{
+            //    Console.Clear();
+            //    for (int i = 1; i <= 12; i++)
+            //    {
+            //        MP += Num;
+            //        Console.WriteLine(MP);
+            //    }
+            //}
+
             #endregion
         }
     }
